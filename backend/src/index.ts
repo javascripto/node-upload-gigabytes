52c2fae2edadd2efd5b387b8c1d6434743cc1931
Yuri Almeida <yurialves2@gmail.com>
2021-02-21 17:18:26 -0300

    lint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,41 @@
 import http, {
   IncomingMessage as Request,
-  ServerResponse as Response
-} from 'http'
-import { AddressInfo } from 'net'
-import { Server as SocketIo } from 'socket.io'
+  ServerResponse as Response,
+} from 'http';
+import { AddressInfo } from 'net';
+import { Server as SocketIo } from 'socket.io';
 
-import Routes from './routes'
+import { logger } from './util';
 
-const PORT = 3000
+import Routes from './routes';
+
+const PORT = 3000;
 
 const handler = (request: Request, response: Response) => {
-  const defaultRoute = async (request: Request, response: Response) => (
-    response.end('Hello\n')
-  )
+  const defaultRoute = async (request: Request, response: Response) =>
+    response.end('Hello\n');
 
+  const routes = new Routes(io);
+  const method = (request.method?.toLocaleLowerCase() || '') as 'post';
+  const chosen = routes[method] || defaultRoute;
 
-  const routes = new Routes(io)
-  const method = (request.method?.toLocaleLowerCase() || '') as 'post'
-  const chosen = routes[method] || defaultRoute
+  return chosen.apply(routes, [request, response]);
+};
 
-  return chosen.apply(routes, [request, response])
-}
-
-const server = http.createServer(handler)
+const server = http.createServer(handler);
 
 const io = new SocketIo(server, {
   cors: {
     origin: '*',
     credentials: false,
   },
-})
+});
 
-io.on('connection', socket => console.log('someone connected', socket.id))
-
+io.on('connection', (socket) => logger.info('someone connected', socket.id));
 
 const onServerInit = () => {
-  const { address, port } = server.address() as AddressInfo
-  console.log(`app running at http://${address}:${port}`)
-}
+  const { address, port } = server.address() as AddressInfo;
+  console.log(`app running at http://${address}:${port}`);
+};
 
-server.listen(PORT, onServerInit)
+server.listen(PORT, onServerInit);
