318c41a17e041ca9f7d1f0886711690298c756d2
Yuri Almeida <yurialves2@gmail.com>
2021-02-21 17:18:14 -0300

    using upload handler

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,29 +1,40 @@
-import url from 'url'
+import url from 'url';
 
-import {
-  IncomingMessage as Request,
-  ServerResponse as Response
-} from 'http'
-import { Server as SocketIo } from 'socket.io'
+import { IncomingMessage as Request, ServerResponse as Response } from 'http';
+import { Server as SocketIo } from 'socket.io';
+
+import UploadHandler from './UploadHandler';
+import { logger, pipelineAsync } from './util';
 
 export default class Routes {
   constructor(private io: SocketIo) {}
 
   async post(request: Request, response: Response) {
-    const { headers } = request
-    const { query: { socketId } } = url.parse(request.url!, true)
-    const redirectTo = headers.origin!
+    const { headers } = request;
+    const {
+      query: { socketId },
+    } = url.parse(request.url!, true);
+    const redirectTo = headers.origin!;
 
-    this.io.to(socketId as string).emit('file-uploaded', 5e6)
+    const uploadHandler = new UploadHandler(this.io, socketId as string);
 
-    const onFinish = (response: Response, redirectTo: string) => {
-      response.writeHead(303, {
-        Connection: 'close',
-        Location: `${redirectTo}?msg=Files uploaded with success!`
-      })
-      response.end()
+    function onFinish(response: Response, redirectTo: string) {
+      return () => {
+        response.writeHead(303, {
+          Connection: 'close',
+          Location: `${redirectTo}?msg=Files uploaded with success!`,
+        });
+        response.end();
+      };
     }
 
-    return onFinish(response, redirectTo)
+    const busboyInstance = uploadHandler.registerEvents(
+      headers,
+      onFinish(response, redirectTo)
+    );
+
+    await pipelineAsync(request, busboyInstance);
+
+    logger.info('Request finished with success!');
   }
 }
